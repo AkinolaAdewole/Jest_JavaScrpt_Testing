8d6207b693a6d4080bd725c898e04c0a3dbaac2a
Akinola <adewoleakinola24@gmail.com>
2023-06-27 20:39:42 +0100

    Jest test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,6 @@
 const {generateText} =require('./util');
 
-test('should output name and age',()=>{})
+test('should output name and age',()=>{
+    const test = generateText('Adewole', 21);
+    expect(text).toBe('Adewole (21 years old)');
+})
