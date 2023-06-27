5ce2f1b2bc97e0f918b1a397067d2758c7310b2f
Akinola <adewoleakinola24@gmail.com>
2023-06-27 21:33:05 +0100

    Jest test continues

--- util.test.js
+++ util.test.js
@@ -1,6 +1,18 @@
 const {generateText} =require('./util');
 
+
+// Unit Test
 test('should output name and age',()=>{
-    const test = generateText('Adewole', 21);
+    const text = generateText('Adewole', 21);
     expect(text).toBe('Adewole (21 years old)');
+})
+
+
+// Integration Test
+test('should output data-less text', ()=>{
+    const text =generateText('', null);
+    expect(text).toBe(' (null years old)');
+          //OR
+    const text2 =generateText();
+    expect(text2).toBe('undefined (undefined years old)')
 })
